e7f1acf1a0865051d0de611d04a17557088ce0e7
anetastankovska <anetastankovskaane@gmail.com>
2022-04-04 18:17:19 +0200

    Add project files.

---
+++ Class01/SEDC.Oop.Class01.Exercise1/SEDC.Oop.Class01.Exercise1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+
+        TextCounter();
+
+        Console.ReadLine();
+
+    }
+
+    static int TextCounter()
+    {
+        Console.WriteLine("Enter a text below");
+        List<string> textList = new List<string>();
+
+        string userInput = string.Empty;
+
+
+        while (userInput != "x")
+        {
+            userInput = Console.ReadLine();
+            textList.Add(userInput);
+        }
+
+        Console.WriteLine("Please enter a text");
+        string userText = Console.ReadLine();
+
+        int counter = 0;
+        foreach (string item in textList)
+        {
+            if () { }
+        }
+
+    }
+}

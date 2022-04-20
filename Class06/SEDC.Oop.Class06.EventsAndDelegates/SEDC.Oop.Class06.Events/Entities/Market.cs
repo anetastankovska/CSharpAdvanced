e6a0233e3a55b57ea3e8c3af833755f45449c6cd
anetastankovska <anetastankovskaane@gmail.com>
2022-04-20 20:34:35 +0200

    windows forms example

---
+++ Class06/SEDC.Oop.Class06.EventsAndDelegates/SEDC.Oop.Class06.Events/Entities/Market.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Oop.Class06.Events.Entities
+{
+    public class Market
+    {
+        public delegate void PromotionSender(ProductType productType, string name);
+
+        public event PromotionSender PromotionSenderEvent;
+
+        public ProductType CurrentProductType { get; set; }
+
+        public List<string> Emails { get; set; }
+
+        public string Name { get; set; }
+
+        public Market()
+        {
+            Emails = new List<string>();
+        }
+
+        public void SubscribeToPromotion(PromotionSender eventHandler, string email)
+        {
+            PromotionSenderEvent += eventHandler;
+            Emails.Add(email);
+        }
+
+        public void Send()
+        {
+            PromotionSenderEvent(CurrentProductType, Name);
+        }
+
+        public void Unsubscribe(PromotionSender eventHandler, string email)
+        {
+            PromotionSenderEvent -= eventHandler;
+            string emailToDelete = Emails.FirstOrDefault(x => x == email);
+            Emails.Remove(emailToDelete);
+        }
+
+    }
+
+    public enum ProductType
+    {
+        Food,
+        Cosmetics,
+        Electronic,
+        Other
+    }
+}
---
+++ Class06/SEDC.Oop.Class06.EventsAndDelegates/SEDC.Oop.Class06.Events/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SEDC.Oop.Class06.Events.Entities;
+
+Market tineks = new Market()
+{
+    Name = "Tineks",
+    CurrentProductType = ProductType.Food
+};
+
+Market reptil = new Market()
+{
+    Name = "Reptil",
+    CurrentProductType = ProductType.Cosmetics
+};
+
+Market neptun = new Market()
+{
+    Name = "Neptun",
+    CurrentProductType = ProductType.Electronic
+};
+
+User ivan = new User("Ivan", "Djikovski", 34, "ivan.djikovski@gmail.com");
+User vlatko = new User("Vlatko", "Tasevski", 25, "vlatko.tasevski@gmail.com");
+User aneta = new User("Aneta", "Stankovska", 30, "aneta.stankovska@gmail.com");
+User stefan = new User("Stefan", "Ivanovski", 31, "stefan.ivanovski@gmail.com");
+
+tineks.SubscribeToPromotion(ivan.ReadPromotion, ivan.Email);
+tineks.SubscribeToPromotion(vlatko.ReadPromotion, vlatko.Email);
+tineks.SubscribeToPromotion(stefan.ReadPromotion, stefan.Email);
+
+Console.WriteLine("============================================");
+
+neptun.SubscribeToPromotion(aneta.ReadPromotion, aneta.Email);
+neptun.SubscribeToPromotion(stefan.ReadPromotion, stefan.Email);
+
+Console.WriteLine("============================================");
+
+tineks.Unsubscribe(vlatko.ReadPromotion, vlatko.Email);
+
+tineks.Send();
+
+reptil.SubscribeToPromotion(ReadPromotion, "Console writeline");
+//reptil.SubscribeToPromotion((type, marketName) =>
+//{
+//    Console.WriteLine($"Subscription from anonimous method: Promotion for {type} at {marketName}") , "fake email";
+//});
+
+reptil.Send();
+
+static void ReadPromotion(ProductType type, string marketName)
+{
+    Console.WriteLine($"Subscription from main method: Promotion for {type} at {marketName}");
+}
+
+
+

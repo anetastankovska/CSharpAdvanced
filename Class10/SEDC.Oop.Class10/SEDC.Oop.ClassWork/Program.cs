54bfe8bac3160439e24d22e92c3487958f873d23
anetastankovska <anetastankovskaane@gmail.com>
2022-05-07 19:16:30 +0200

    Added class code

---
+++ Class10/SEDC.Oop.Class10/SEDC.Oop.ClassWork/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SEDC.Oop.ClassWork;
+
+Console.WriteLine("Hello, World!");
+
+var playerOne = RPS.Paper;
+var playerTwo = RPS.Rock;

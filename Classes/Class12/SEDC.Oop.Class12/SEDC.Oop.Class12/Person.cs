60b32b01d00e013d5f4cde446706bd2d1e4af46f
anetastankovska <anetastankovskaane@gmail.com>
2022-05-21 09:45:35 +0200

    Added code

--- Classes/Class12/SEDC.Oop.Class12/SEDC.Oop.Class12/Person.cs
+++ Classes/Class12/SEDC.Oop.Class12/SEDC.Oop.Class12/Person.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+
 
 namespace SEDC.Oop.Class12
 {
